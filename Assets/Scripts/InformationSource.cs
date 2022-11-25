77129a3eebbb839e708c0fcad7eb71cc9ae642e9
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2022-11-25 14:31:49 +0200

    add sprites

--- Assets/Scripts/InformationSource.cs
+++ Assets/Scripts/InformationSource.cs
@@ -1,18 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// A component defining the node that this source is attached to as being an information source,
+/// either misinformation or reliable
+/// </summary>
 public class InformationSource : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public NodeType sourceType;
+    private Node _nodeAttachedTo;
+    public int informationPower;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        // Every update cycle, we check how many nodes are we connected to to determine the power output
+        var nodesConnectedTo = _nodeAttachedTo.connectedNodes.Count;
         
+        // Tell each connected node what the current output is from this node
+        
+        // Sum all directly sourced strengths to each node and set the level snd type of the node
+        
+        // Spread the information outwards based on each nodes strength
     }
 }
---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,27 @@
+using System;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+
+    private void AddNode()
+    {
+    }
+
+    private void AddInformationSource(NodeType type)
+    {
+    }
+
+    private void RemoveInformationSource(NodeType type)
+    {
+    }
+}
