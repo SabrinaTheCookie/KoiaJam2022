711626841ecb8c469f92e9d70eb24158ef9dcb55
Brayden Cooke <Braydencooke@hotmail.com>
2022-11-26 09:35:14 +1300

    Added link gizmos

--- Assets/Link.cs
+++ Assets/Link.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -58,6 +59,7 @@ public class Link : MonoBehaviour
         //Also hitting nodes...
         //Check overlap - if overlapping, destroy this link and undo the connection (That has been established)
         Collider2D[] hits = Physics2D.OverlapBoxAll(transform.position, transform.localScale, transform.rotation.z, linkLayerMask);
+        
 
         Debug.Log(hits.Length);
         foreach (Collider2D hit in hits)
@@ -91,7 +93,7 @@ public class Link : MonoBehaviour
         //Remove connection from end node
         endNode.BreakConnection(startNode);
         
-        Debug.Log("Destroy me daddy");
+        Debug.Log("Destroying link - Start Node: " + startNode + " | End Node: " + endNode);
         //Destroy this link
         Destroy(gameObject);
     }
@@ -111,5 +113,12 @@ public class Link : MonoBehaviour
         var angle = Vector2.Angle(new Vector2(0.0f, 1.0f), new Vector2(linkDirection.x, linkDirection.y));
         if (linkDirection.x > 0.0f) angle = 360.0f - angle;
         transform.Rotate(0,0, angle);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.matrix = transform.localToWorldMatrix;
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(Vector3.zero, Vector3.one);
     }
 }
