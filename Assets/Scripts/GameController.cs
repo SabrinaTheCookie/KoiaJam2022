c157f3d41885b47bb7e9d1d1df342e4b4bbd9a4b
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2022-11-25 12:05:34 +0200

    add some spawning basics and game bounds limits

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,6 +5,14 @@ public class GameController : MonoBehaviour
 {
     public static event Action StartGame;
 
+    // Instance var for singleton access
+    public static GameController Instance { get; private set; }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
     private void OnStartGame()
     {
         StartGame?.Invoke();
--- Assets/Scripts/NodeSpawner.cs
+++ Assets/Scripts/NodeSpawner.cs
@@ -1,16 +1,26 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class NodeSpawner : MonoBehaviour
 {
     // Instance var for singleton access
     public static NodeSpawner Instance { get; private set; }
 
-    [SerializeField] public GameObject nodePrefab;
+    [SerializeField] private GameObject nodePrefab;
+    public float nodeSizeMult = 1f;
+    private float _nodeRadius;
+    public int maxNodeConnections = 3;
+    public int minTotalNodes;
+    public int maxTotalNodes;
+    private List<Node> _allNodes;
 
     private void Awake()
     {
         Instance = this;
+
+        _nodeRadius = nodePrefab.GetComponent<Renderer>().bounds.size.x / 2 * nodeSizeMult;
     }
 
     private void OnEnable()
@@ -18,8 +28,23 @@ public class NodeSpawner : MonoBehaviour
         GameController.StartGame += SetUpNodes;
     }
 
+    private void Start()
+    {
+        SetUpNodes();
+    }
+
     private void SetUpNodes()
     {
         // Set up the nodes in the game world
+        var nodesToPlace = Random.Range(minTotalNodes, maxTotalNodes);
+
+        Limits boundaries = Limits.GetLimits(_nodeRadius);
+        
+        Debug.Log(boundaries);
+
+        // Randomly generate locations for all the nodes. Ensure none overlap
+        for (var i = 0; i < nodesToPlace; i++)
+        {
+        }
     }
 }
